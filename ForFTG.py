4fba4c51aba82c0b7196b49d0112d461c2965bfb
SpeeDuwaGon13 <vovkapedenko@gmail.com>
2021-09-19 22:14:16 +0300

    release

--- ForFTG.py
+++ ForFTG.py
@@ -1,31 +1,46 @@
 from .. import loader, utils
 from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 
 def register(cb):
-    cb(ProstoPrivetMod())
+    cb(TikTokDLMOD())
 
-class ProstoPrivetMod(loader.Module):
-    """тестовый модуль"""
-    strings = {'name': 'FirstPosition'}
+class TikTokDLMOD(loader.Module):
+    """Скачивает ТикТоки без водяного знака"""
+    strings = {'name': 'TikTokDlByVova'}
 
-    async def vsaycmd(self, message):
-        """Используй .vsay <аргумент> (abme, ths)"""
+    async def tiktokcmd(self, message):
+        """.tiktok <аргумент или реплай>"""
+        chat = "@SaveOFFbot"
         text = utils.get_args_raw(message)
+        reply = await message.get_reply_message()
 
-        if not text:
-            await message.edit("<b>вы забыли вписать аргумент</b>")
+        if not text and not reply:
+            await message.edit("<b>Нет аргумента или реплая</b>")
             return
-
-        await message.edit("<b>Одно мгновение...</b>")
-
-        if text == "abme":
-            await message.edit("<b>Я начинающий создатель модулей</b>")
-            return
-
-        elif text == "ths":
-            await message.edit("<b>Это мой первый тестовый модуль :D</b>")
-            return
-
-        else:
-            await message.edit("<b>Не существует такого аргумента. Черть</b>")
+        await message.edit("<b>Пожалуйста, поождите...</b>")
+        async with message.conversation(chat) as conv:
+            if text:
+                try:
+                    response = conv.wait_event(events.NewMessage(incoming=True, from_users=1825028508))
+                    await message.client.send_message(chat, text)
+                    response = await response
+                except YouBlockedUserError:
+                    await message.edit("<b>Разблокируй бота @SaveOFFbot !</b>")
+                    return
+            else:
+                try:
+                    user = await utils.get_user(reply)
+                    response = conv.wait_event(events.NewMessage(incoming=True, from_users=1825028508))
+                    await message.client.send_message(chat, f"{reply.raw_text} (c) {user.first_name}")
+                    response = await response
+                except YouBlockedUserError:
+                    await message.edit("<b>Разблокируй бота @SaveOFFbot !</b>")
+                    return
+        if response.media:
+            await message.client.send_file(message.to_id, response.media, reply_to=reply.id if reply else None)
+            await message.delete()
+        if response.text:
+            await message.client.send_message(message.to_id, f"<b> {response.text} </b>")
+            await.message.delete()
