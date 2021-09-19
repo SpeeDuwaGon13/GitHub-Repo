9fbbde21835d98a43c9aa8c5bcb259b7e1a53fbe
SpeeDuwaGon13 <vovkapedenko@gmail.com>
2021-09-19 15:34:38 +0300

    pre beta1

--- ForFTG.py
+++ ForFTG.py
@@ -1,2 +1,28 @@
-print("hello world!")
-print("test")
+from .. import loader, utils
+from telethon import events
+
+
+def register(cb):
+    cb(ProstoPrivetMod())
+
+class ProstoPrivetMod(loader.Modue):
+    """тестовый модуль"""
+    strings = {'name': 'FirstPosition'}
+
+    async def vsaycmd(self, message):
+        """Используй .vsay <аргумент> (abme, ths)"""
+        text = utils.get_args_raw(message)
+
+        if not text:
+            await message.edit("<b>вы забыли вписать аргумент</b>")
+            return
+
+        await message.edit("<b>Одно мгновение...</b>")
+
+        if text == "abme":
+            await message.edit("<b>Я начинающий создатель модулей</b>")
+            return
+
+        if text == "ths":
+            await message.edit("<b>Это мой первый тестовый модуль :D</b>")
+            return
